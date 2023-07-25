c0058592e68131ae433e63436317b8bc351febcb
denessolti <denes.solti@sophos.com>
2023-07-25 09:09:15 +0200

    project initials, PathSplitter class

---
+++ Router.Tests/PathSplitterTests.cs
@@ -0,0 +1,71 @@
+﻿using NUnit.Framework;
+
+namespace Router.Tests
+{
+    using Internals;
+
+    [TestFixture]
+    public class PathSplitterTests
+    {
+        [TestCase("/cica/")]
+        [TestCase("/cica")]
+        [TestCase("cica/")]
+        [TestCase("/cica/mica/")]
+        [TestCase("/cica/mica")]
+        [TestCase("cica/mica/")]
+        public void SplitShouldNotTakeLeadingAndTrailingSeparatorsIntoAccount(string input, string[] expected)
+        {
+        }
+
+        [Test]
+        public void SplitShouldHandleEmptyPath()
+        {
+        }
+
+        // Too short hex
+        [TestCase("%")]
+        [TestCase("%/")]
+        [TestCase("%/mica")]
+        [TestCase("cica/%")]
+        [TestCase("cica/%/")]
+        [TestCase("cica/%/mica")]
+        [TestCase("/%")]
+        [TestCase("/%/")]
+        [TestCase("/%/mica")]
+        [TestCase("/cica/%")]
+        [TestCase("/cica/%/")]
+        [TestCase("/cica/%/mica")]
+
+        [TestCase("%A")]
+        [TestCase("%A/")]
+        [TestCase("%A/mica")]
+        [TestCase("cica/%A")]
+        [TestCase("cica/%A/")]
+        [TestCase("cica/%A/mica")]
+        [TestCase("/%A")]
+        [TestCase("/%A/")]
+        [TestCase("/%A/mica")]
+        [TestCase("/cica/%A")]
+        [TestCase("/cica/%A/")]
+        [TestCase("/cica/%A/mica")]
+
+        // Invalid hex
+        [TestCase("/%AÁ")]
+        [TestCase("/%AÁ/")]
+        [TestCase("/%AÁ/mica")]   
+        [TestCase("/cica/%AÁ")]
+        [TestCase("/cica/%AÁ/")]
+        [TestCase("/cica/%AÁ/mica")]
+
+        [TestCase("%AÁ")]
+        [TestCase("%AÁ/")]
+        [TestCase("%AÁ/mica")]
+        [TestCase("cica/%AÁ")]
+        [TestCase("cica/%AÁ/")]
+        [TestCase("cica/%AÁ/mica")]
+        public void SplitShouldThrowOnInvalidHex(string input)
+        {
+            Assert.Throws<ArgumentException>(() => PathSplitter.Split(input).ToList());
+        }
+    }
+}
---
+++ Router/Private/PathSplitter.cs
@@ -0,0 +1,57 @@
+﻿/********************************************************************************
+* PathSplitter.cs                                                               *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Router.Internals
+{
+    using Properties;
+
+    internal static class PathSplitter
+    {
+        /// <summary>
+        /// Splits the given path converting hex values if necessary.
+        /// </summary>
+        public static IEnumerable<string> Split(string path)
+        {
+            char[] buffer = new char[path.Length];
+            int pos = 0;
+
+            for (int i = 0; i < path.Length; i++)
+            {
+                char c = path[i];
+
+                switch (c)
+                {
+                    case '/':
+                        //
+                        // Skip leading separator
+                        //
+
+                        if (i > 0)
+                        {
+                            yield return new string(buffer, 0, pos);
+                            pos = 0;
+                        }
+                        continue;
+                    case '%':
+                        if (path.Length - i <= 2 || !int.TryParse(path.Substring(i + 1, 2), NumberStyles.HexNumber, null, out int chr))
+                            throw new ArgumentException(Resources.INVALID_PATH, nameof(path));
+
+                        c = (char) chr;
+                        i += 2;
+                        break;
+                }
+                buffer[pos++] = c;
+
+            }
+
+            if (pos > 0)
+                yield return new string(buffer, 0, pos);
+        } 
+    }
+}
