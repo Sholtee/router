dca2ddacad9d47a37bdfc08c74f46335aa60d70b
denessolti <denes.solti@sophos.com>
2023-07-30 12:53:50 +0200

    introduce SwitchBuilder.DefineCase() method

--- Router/Private/SwitchBuilder.cs
+++ Router/Private/SwitchBuilder.cs
@@ -3,17 +3,22 @@
 *                                                                               *
 * Author: Denes Solti                                                           *
 ********************************************************************************/
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 
 namespace Router.Internals
 {
     /// <summary>
     /// Builds the switch statement which does the actual routing.
     /// <code>
-    /// void Route(string path)
+    /// TResponse Route(string path, TRequest request, TUserData userData)
     /// {
     ///     Dictionary&lt;string, object?&gt; paramz = new();
+    ///     object converted;
     ///     
     ///     using(IEnumerator&lt;string&gt; segments = PathSplitter.Split(path).GetEnumerator())
     ///     {
@@ -25,31 +30,177 @@ namespace Router.Internals
     ///                 {
     ///                     if (segments.Current == "mica")
     ///                     {
-    ///                         CicaMicaHandler(paramz, path); // "/cica/mica" defined
-    ///                         return;
+    ///                         if (segments.MoveNext())
+    ///                         {
+    ///                             return DefaultHandler(request, userData, path);
+    ///                         }
+    /// 
+    ///                         return CicaMicaHandler(request, paramz, userData, path); // "/cica/mica" defined
     ///                     }
-    ///                     else if (intParser(segments.Current, out int val))
+    ///                     
+    ///                     if (intParser(segments.Current, out converted))
     ///                     {
+    ///                         if (segments.MoveNext())
+    ///                         {
+    ///                             return DefaultHandler(request, userData, path);
+    ///                         }
+    /// 
     ///                         paramz.Add("id", val);
-    ///                         CicaIdHandler(paramz, path); // "/cica/{id:int}" defined
-    ///                         return;
+    ///                         return CicaIdHandler(request, paramz, userData, path); // "/cica/{id:int}" defined
     ///                     }
+    ///                     
+    ///                     return DefaultHandler(request, userData, path);  // neither "/cica/mica" nor "/cica/{id:int}"
     ///                 }
     ///                 
-    ///                 CicaHandler(paramz, path); // "/cica" defined
-    ///                 return;
+    ///                 return CicaHandler(request, paramz, userData, path); // "/cica" defined
     ///             }
+    ///             
+    ///             return DefaultHandler(request, userData, path);  // not "/cica[/..]"
     ///         }
     ///         
-    ///         DefaultHandler(path);  // "/" is not defined
-    ///         return;
+    ///         return RootHandler(request, paramz, userData, path);  // "/" is defined
     ///     }
     /// }
     /// </code>
     /// </summary>
-    internal class SwitchBuilder
+    internal class SwitchBuilder<TRequest, TUserData, TResponse>
     {
+        private static readonly MethodInfo FMoveNext =
+        (
+            (MethodCallExpression) 
+            (
+                (Expression<Action<IEnumerator<string>>>) 
+                (
+                    static enumerator => enumerator.MoveNext()
+                )
+            ).Body
+        ).Method;
+
+        private static readonly MethodInfo FAddParam =
+        (
+            (MethodCallExpression)
+            (
+                (Expression<Action<Dictionary<string, object?>>>)
+                (
+                    static dict => dict.Add(null!, null)
+                )
+            ).Body
+        ).Method;
+
+        private static readonly MethodInfo FDispose =
+        (
+            (MethodCallExpression)
+            (
+                (Expression<Action<IEnumerator<string>>>)
+                (
+                    static enumerator => enumerator.Dispose()
+                )
+            ).Body
+        ).Method;
+
+        private static readonly PropertyInfo FCurrent = (PropertyInfo) 
+        (
+            (MemberExpression) 
+            (
+                (Expression<Func<IEnumerator<string>, string>>)
+                (
+                    static enumerator => enumerator.Current
+                )
+            ).Body
+        ).Member;
+
         private readonly RouteParser FRouteParser;
+
+
+        private static IEnumerator<string> GetSegments(string path) => PathSplitter.Split(path).GetEnumerator();
+
+        private readonly DefaultHandler<TRequest, TUserData, TResponse> FDefaultHandler;
+
+        private sealed class Junction
+        {
+            public IEnumerable<Junction> Walk()
+            {
+                yield break;
+            }
+
+            public RouteSegment? Segment { get; }  // null at "/"
+
+            public Handler<TRequest, TUserData, TResponse>? Handler { get; }  // may be null at "/"
+        }
+
+        private Expression DefineCase
+        (
+            ParameterExpression segments,
+            ParameterExpression request,
+            ParameterExpression paramz,
+            ParameterExpression userData,
+            ParameterExpression path,
+            ParameterExpression converted,
+            Junction junction
+        )
+        {
+            Expression tryProcessNextJunction = Expression.IfThen
+            (
+                Expression.Call(segments, FMoveNext),
+                Expression.Block
+                (
+                    type: typeof(TResponse),
+                    junction
+                        .Walk()
+                        // next junctions...
+                        .Select(junction => DefineCase(segments, request, paramz, userData, path, converted, junction))
+                        // default handler
+                        .Append(Expression.Invoke(Expression.Constant(FDefaultHandler), request, userData, path))
+                )
+            );
+
+
+            if (junction.Segment is null)  // root node, no segment
+            {
+                return Expression.Block
+                (
+                    type: typeof(TResponse),
+                    tryProcessNextJunction,
+                    junction.Handler is not null
+                        ? Expression.Invoke(Expression.Constant(junction.Handler), request, paramz, userData, path)
+                        : Expression.Invoke(Expression.Constant(FDefaultHandler), request, userData, path)
+                );
+            }
+
+            Debug.Assert(junction.Handler is not null, "Handler cannot be null");
+
+            return junction.Segment.Converter is null
+                ? Expression.IfThen
+                (
+                    Expression.Equal
+                    (
+                        Expression.Property(segments, FCurrent),
+                        Expression.Constant(junction.Segment.Name)
+                    ),
+                    Expression.Block
+                    (
+                        type: typeof(TResponse),
+                        tryProcessNextJunction,
+                        Expression.Invoke(Expression.Constant(junction.Handler), request, paramz, userData, path)
+                    )
+                )
+                : Expression.IfThen
+                (
+                    Expression.Invoke
+                    (
+                        Expression.Constant(junction.Segment.Converter),
+                        Expression.Property(segments, FCurrent),
+                        converted
+                    ),
+                    Expression.Block
+                    (
+                        type: typeof(TResponse),
+                        tryProcessNextJunction,
+                        Expression.Call(paramz, FAddParam, Expression.Constant(junction.Segment.Name), converted),
+                        Expression.Invoke(Expression.Constant(junction.Handler), request, paramz, userData, path)
+                    )
+                );
+        }
 
         public void AddRoute(string route)
         {
---
+++ Router/Public/Delegates.cs
@@ -0,0 +1,36 @@
+﻿/********************************************************************************
+* Delegates.cs                                                                  *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System.Collections.Generic;
+
+namespace Router
+{
+    /// <summary>
+    /// Tries to convert the input string to a given type.
+    /// </summary>
+    /// <param name="input">The input string</param>
+    /// <param name="userData">User provided data, comes from the route template.</param>
+    /// <param name="value">The converted value if the conversion was successful</param>
+    public delegate bool TryConvert(string input, string? userData, out object? value);
+
+    /// <summary>
+    /// Handler for unknown routes.
+    /// </summary>
+    /// <param name="request">The request object.</param>
+    /// <param name="userData">User provided custom data.</param>
+    /// <param name="path">Path led to here</param>
+    /// <returns>The response object.</returns>
+    public delegate TResponse DefaultHandler<TRequest, TUserData, TResponse>(TRequest request, TUserData userData, string path);
+
+    /// <summary>
+    /// Handler for known routes.
+    /// </summary>
+    /// <param name="request">The request object.</param>
+    /// <param name="paramz">The request parameters.</param>
+    /// <param name="userData">User provided custom data.</param>
+    /// <param name="path">Path led to here</param>
+    /// <returns>The response object.</returns>
+    public delegate TResponse Handler<TRequest, TUserData, TResponse>(TRequest request, IReadOnlyDictionary<string, object?> paramz, TUserData userData, string path);
+}
