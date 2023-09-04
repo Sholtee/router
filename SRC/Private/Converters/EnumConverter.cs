4be3623e79a669044e7d21924fc235b667e4f1c4
denes.solti <sodnaatx@gmail.com>
2023-09-04 07:02:05 +0200

    NETSTANDARD2_0 support I

--- SRC/Private/Converters/EnumConverter.cs
+++ SRC/Private/Converters/EnumConverter.cs
@@ -9,10 +9,33 @@ using System.Linq;
 
 namespace Solti.Utils.Router.Internals
 {
+#if NETSTANDARD2_0
+    using System.Diagnostics;
+    using System.Linq.Expressions;
+    using System.Reflection;
+
+    using Primitives;
+#endif
     using static Properties.Resources;
 
     internal sealed class EnumConverter : ConverterBase
     {
+#if NETSTANDARD2_0
+        private static readonly MethodInfo FTryParseGen =
+        (
+            (MethodCallExpression)
+            (
+                (Expression<Action<SplitOptions>>)
+                (
+                    static o => Enum.TryParse(null!, true, out o)
+                )
+            ).Body
+        ).Method.GetGenericMethodDefinition();
+
+        private delegate bool TryParse(string input, out object? value);
+
+        private readonly TryParse FTryParse;
+#endif
         public Type EnumType { get; }
 
         public EnumConverter(string? style): base(style)
@@ -35,6 +58,43 @@ namespace Solti.Utils.Router.Internals
                 throw new ArgumentException(string.Format(Culture, INVALID_FORMAT_STYLE, style), nameof(style));
 
             EnumType = hits[0];
+#if NETSTANDARD2_0
+            ParameterExpression
+                input  = Expression.Parameter(typeof(string), nameof(input)),
+                output = Expression.Parameter(typeof(object).MakeByRefType(), nameof(output)),
+                ret    = Expression.Variable(EnumType, nameof(ret));
+
+            LabelTarget exit = Expression.Label(typeof(bool), nameof(exit));
+
+            Expression<TryParse> tryParseExpr = Expression.Lambda<TryParse>
+            (
+                Expression.Block
+                (
+                    variables: new ParameterExpression[] { ret },
+                    Expression.IfThen
+                    (
+                        Expression.Call
+                        (
+                            FTryParseGen.MakeGenericMethod(EnumType),
+                            input,
+                            Expression.Constant(true),  // ignoreCase
+                            ret
+                        ),
+                        ifTrue: Expression.Block
+                        (
+                            Expression.Assign(output, Expression.Convert(ret, typeof(object))),
+                            Expression.Goto(exit, Expression.Constant(true))
+                        )
+                    ),
+                    Expression.Label(exit, Expression.Constant(false))
+                ),
+                input,
+                output
+            );
+            Debug.WriteLine(tryParseExpr.GetDebugView());
+
+            FTryParse = tryParseExpr.Compile();
+#endif
         }
 
         public override bool ConvertToString(object? input, out string? value)
@@ -50,6 +110,10 @@ namespace Solti.Utils.Router.Internals
         }
 
         public override bool ConvertToValue(string input, out object? value) =>
+#if NETSTANDARD2_0
+            FTryParse(input, out value);
+#else
             Enum.TryParse(EnumType, input, ignoreCase: true, out value);
+#endif
     }
 }
