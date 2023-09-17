164a7996d809e1e2c1ef15683695b741cf84ecc4
denes.solti <sodnaatx@gmail.com>
2023-09-17 09:21:14 +0200

    measure AsyncRouter performance

--- PERF/Routing.cs
+++ PERF/Routing.cs
@@ -4,6 +4,7 @@
 * Author: Denes Solti                                                           *
 ********************************************************************************/
 using System.Linq;
+using System.Threading.Tasks;
 
 using BenchmarkDotNet.Attributes;
 
@@ -22,31 +23,51 @@ namespace Solti.Utils.Router.Perf
 
         public Router Router { get; set; } = null!;
 
+        private string CreateTemplate()
+        {
+            int paramIndex = 0;
+
+            return "/" + string.Join
+            (
+                "/",
+                Enumerable
+                    .Repeat("segment", SegmentCount)
+                    .Select((segment, i) => HasParams && i % 2 == 0 ? $"{{param{paramIndex++}:str}}" : segment)
+            );
+        }
+
+        private void SetInput() => Input = "/" + string.Join("/", Enumerable.Repeat("segment", SegmentCount));
+
         [GlobalSetup(Target = nameof(Route))]
         public void SetupRoute()
         {
             RouterBuilder bldr = new(handler: (_, _) => true);
 
-            int paramIndex = 0;
-
-            bldr.AddRoute
-            (
-                "/" + string.Join
-                (
-                    "/",
-                    Enumerable
-                        .Repeat("segment", SegmentCount)
-                        .Select((segment, i) => HasParams && i % 2 == 0 ? $"{{param{paramIndex++}:str}}" : segment)
-                ),
-                handler: (_, _) => true
-            );
+            bldr.AddRoute(CreateTemplate(), handler: (_, _) => true);
 
             Router = bldr.Build();
 
-            Input = "/" + string.Join("/", Enumerable.Repeat("segment", SegmentCount));
+            SetInput();
         }
 
         [Benchmark]
         public void Route() => Router(null, Input);
+
+        public AsyncRouter AsyncRouter { get; set; } = null!;
+
+        [GlobalSetup(Target = nameof(AsyncRoute))]
+        public void SetupAsyncRoute()
+        {
+            AsyncRouterBuilder bldr = AsyncRouterBuilder.Create(handler: (_, _) => Task.FromResult(true));
+
+            bldr.AddRoute(CreateTemplate(), handler: (_, _) => Task.FromResult(true));
+
+            AsyncRouter = bldr.Build();
+
+            SetInput();
+        }
+
+        [Benchmark]
+        public async Task<object?> AsyncRoute() => await AsyncRouter(null, Input);
     }
 }
