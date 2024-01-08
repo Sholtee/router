51f8e304d1e72cf566b705c9e3be6c23cd9eab4d
denes.solti <sodnaatx@gmail.com>
2024-01-08 09:08:51 +0100

    add some AsyncRouterBuilderAddRouteExtensions.AddRoute() overloads

--- SRC/Public/Extensions/AsyncRouterBuilderAddRouteExtensions.cs
+++ SRC/Public/Extensions/AsyncRouterBuilderAddRouteExtensions.cs
@@ -47,13 +47,45 @@ namespace Solti.Utils.Router.Extensions
         /// </summary>
         /// <param name="self"><see cref="AsyncRouterBuilder"/> instance.</param>
         /// <param name="route">Route to be registered.</param>
+        /// <param name="handler">MEthod accepting requests on the given route. You may pass async and sync callbacks as well.</param>
+        /// <param name="splitOptions">Specifies how to split the <paramref name="route"/>.</param>
+        /// <param name="methods">Accepted HTTP methods for this route. If omitted "GET" will be used.</param>
+        /// <exception cref="ArgumentException">If the route already registered.</exception>
+        public static void AddRoute(this AsyncRouterBuilder self, string route, MethodInfo handler, SplitOptions splitOptions, params string[] methods) => self.AddRoute
+        (
+            RouteTemplate.Parse(route, self.Converters, splitOptions),
+            handler,
+            methods
+        );
+
+        /// <summary>
+        /// Registers a new route.
+        /// </summary>
+        /// <param name="self"><see cref="AsyncRouterBuilder"/> instance.</param>
+        /// <param name="route">Route to be registered.</param>
+        /// <param name="handler">MEthod accepting requests on the given route. You may pass async and sync callbacks as well.</param>
+        /// <param name="methods">Accepted HTTP methods for this route. If omitted "GET" will be used.</param>
+        /// <exception cref="ArgumentException">If the route already registered.</exception>
+        public static void AddRoute(this AsyncRouterBuilder self, string route, MethodInfo handler, params string[] methods) => self.AddRoute
+        (
+            route,
+            handler,
+            SplitOptions.Default,
+            methods
+        );
+
+        /// <summary>
+        /// Registers a new route.
+        /// </summary>
+        /// <param name="self"><see cref="AsyncRouterBuilder"/> instance.</param>
+        /// <param name="route">Route to be registered.</param>
         /// <param name="handlerExpr">Function accepting requests on the given route.</param>
         /// <param name="methods">Accepted HTTP methods for this route. If omitted "GET" will be used.</param>
         /// <exception cref="ArgumentException">If the route already registered.</exception>
         public static void AddRoute<TService>(this AsyncRouterBuilder self, ParsedRoute route, Expression<Action<TService>> handlerExpr, params string[] methods) => self.AddRoute
         (
             route,
-            handler: MethodInfoExtractor.Extract(handlerExpr),
+            MethodInfoExtractor.Extract(handlerExpr ?? throw new ArgumentNullException(nameof(handlerExpr))),
             methods
         );
 
@@ -69,8 +101,9 @@ namespace Solti.Utils.Router.Extensions
         public static void AddRoute<TService>(this AsyncRouterBuilder self, string route, Expression<Action<TService>> handlerExpr, SplitOptions splitOptions, params string[] methods) => AddRoute
         (
             self,
-            RouteTemplate.Parse(route, self.Converters, splitOptions),
-            handlerExpr,
+            route,
+            MethodInfoExtractor.Extract(handlerExpr ?? throw new ArgumentNullException(nameof(handlerExpr))),
+            splitOptions,
             methods
         );
 
---
+++ TEST/AsyncRouterBuilderAddRouteExtensionsTests.cs
@@ -0,0 +1,35 @@
+﻿/********************************************************************************
+* AsyncRouterBuilderAddRouteExtensionsTests.cs                                  *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System;
+
+using NUnit.Framework;
+
+namespace Solti.Utils.Router.Extensions.Tests
+{
+    [TestFixture]
+    public class AsyncRouterBuilderAddRouteExtensionsTests
+    {
+        private class MyService
+        {
+            public void MyHandler() { }
+        }
+
+        [Test]
+        public void AddRouteShouldBeNullChecked()
+        {
+            AsyncRouterBuilder bldr = AsyncRouterBuilder.Create();
+
+            Assert.Throws<ArgumentNullException>(() => bldr.AddRoute<MyService>(route: (string) null!, handlerExpr: svc => svc.MyHandler(), SplitOptions.Default));
+            Assert.Throws<ArgumentNullException>(() => bldr.AddRoute<MyService>(route: (ParsedRoute) null!, handlerExpr: svc => svc.MyHandler()));
+
+            Assert.Throws<ArgumentNullException>(() => bldr.AddRoute<MyService>(route: "/", handlerExpr: null!, SplitOptions.Default));
+            Assert.Throws<ArgumentNullException>(() => bldr.AddRoute<MyService>(route: RouteTemplate.Parse("/"), handlerExpr: null!));
+
+            Assert.Throws<ArgumentNullException>(() => bldr.AddRoute(route: "/", handler: null!));
+            Assert.Throws<ArgumentNullException>(() => bldr.AddRoute(route: RouteTemplate.Parse("/"), handler: null!));
+        }
+    }
+}
