948dfa5160f956604208b0976e3052edf2e09004
denes.solti <sodnaatx@gmail.com>
2024-08-04 13:38:39 +0200

    drop unnecessary ThreadStatic field from AsyncRouterBuilderAddRouteExtensions

--- SRC/Public/Extensions/AsyncRouterBuilderAddRouteExtensions.cs
+++ SRC/Public/Extensions/AsyncRouterBuilderAddRouteExtensions.cs
@@ -16,21 +16,10 @@ namespace Solti.Utils.Router.Extensions
     /// </summary>
     public static class AsyncRouterBuilderAddRouteExtensions
     {
-        [ThreadStatic]
-        private static RequestHandlerBuilder? FRequestHandlerBuilder;
-
         /// <summary>
         /// The <see cref="Extensions.RequestHandlerBuilder"/> to be used. The default is <see cref="MsDiRequestHandlerBuilder"/>
         /// </summary>
-        public static RequestHandlerBuilder RequestHandlerBuilder
-        {
-            get
-            {
-                FRequestHandlerBuilder ??= new MsDiRequestHandlerBuilder();
-                return FRequestHandlerBuilder;
-            }
-            set => FRequestHandlerBuilder = value;
-        }
+        public static RequestHandlerBuilder RequestHandlerBuilder { get; set; } = new MsDiRequestHandlerBuilder();
 
         /// <summary>
         /// Registers a new route.
